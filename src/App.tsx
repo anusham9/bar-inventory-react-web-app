919630569e512bdd721ce7dc0d83ce117968de3b
ChzuoL <chenzuoliu@gmail.com>
2024-11-29 21:23:30 -0800

    implemented the navigation sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,22 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SalesTransactionPage from './pages/SalesTransactionPage';
 import ProductInventory from './pages/ProductInventoryPage';
 import EquipmentManagement from './pages/EquipmentPage';
-import logo from './logo.svg';
+import Sidebar from './pages/Sidebar'; // Import the Sidebar component
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ display: 'flex' }}>
       <Router>
-        <Routes>
-          <Route path="/view/sales-transaction" element={<SalesTransactionPage />} />
-          <Route path="/view/product-inventory" element={<ProductInventory />} />
-          <Route path="/view/equipment" element={<EquipmentManagement />} />
-        </Routes>
+        <Sidebar />
+        {/* Main content area */} 
+        <div style={{ marginLeft: '150px', padding: '20px', width: '100%' }}>
+          <Routes>
+            <Route path="/view/sales-transaction" element={<SalesTransactionPage />} />
+            <Route path="/view/product-inventory" element={<ProductInventory />} />
+            <Route path="/view/equipment" element={<EquipmentManagement />} />
+          </Routes>
+        </div>
       </Router>
     </div>
   );
