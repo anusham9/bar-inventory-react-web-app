a78a2eeed90afefb8eda8c5d7fc13bce6a145e66
Agllai Papaj <156248149+agllaip@users.noreply.github.com>
2024-12-02 21:56:22 -0800

    Final Updates

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import DistributorManagement from './pages/DistributorPage';
 import Reservations from './pages/Reservations';
 import Sidebar from './pages/Sidebar'; // Import the Sidebar component
 import './App.css';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         {/* Main content area */}
         <div style={{ marginLeft: '150px', padding: '20px', width: '100%' }}>
           <Routes>
+            <Route path="/login" element={<LoginPage />} />
             <Route path="/view/notifications" element={<NotificationsPage />} />
             <Route
               path="/view/sales-transaction"
